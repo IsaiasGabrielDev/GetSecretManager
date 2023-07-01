deb45f93984236145cab2e10bb8fdc2812da4693
IsaiasGabriel <isaiasgabrielhub@gmail.com>
2023-07-01 14:25:27 -0300

    Adicionar arquivos de projeto.

---
+++ GetSecretManager/Manager/AwsManager.cs
@@ -0,0 +1,43 @@
+﻿namespace GetSecretManager.Manager
+{
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+    using Amazon;
+    using Amazon.S3;
+    using Amazon.SecretsManager;
+    using Amazon.SecretsManager.Model;
+
+    public class AwsManager
+    {
+        public static async Task GetKey(string acessKey, string secretKey, string savefrom, string secretName)
+        {
+            try
+            {
+                var client = new AmazonSecretsManagerClient(acessKey, secretKey, RegionEndpoint.USEast1);
+
+                var request = new GetSecretValueRequest
+                {
+                    SecretId = secretName
+                };
+                var response = await client.GetSecretValueAsync(request);
+
+                Console.WriteLine(response.SecretString);
+
+                string path = savefrom;
+                if (!File.Exists(path))
+                {
+                    // Create a file to write to.
+                    using (StreamWriter sw = File.CreateText(path))
+                    {
+                        sw.WriteLine(response.SecretString);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ GetSecretManager/Program.cs
@@ -0,0 +1,14 @@
+﻿using GetSecretManager.Manager;
+using System;
+using System.Threading.Tasks;
+
+namespace GetSecretManager
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await AwsManager.GetKey(args[0], args[1], args[2], args[3]);
+        }
+    }
+}
